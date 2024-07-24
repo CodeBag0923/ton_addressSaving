a83503c2627d01494ebfd28172ccd1873f911901
CodeBag0923 <codemaven0923@gmail.com>
2024-07-24 15:07:06 -0300

    complete test for smart contract

--- scripts/deployAddressSaver.ts
+++ scripts/deployAddressSaver.ts
@@ -1,9 +1,9 @@
-import { toNano } from '@ton/core';
+import { address, toNano } from '@ton/core';
 import { AddressSaver } from '../wrappers/AddressSaver';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const addressSaver = provider.open(AddressSaver.createFromConfig({}, await compile('AddressSaver')));
+    const addressSaver = provider.open(AddressSaver.createFromConfig({manager:address("EQC4tYtumFgvZpAi6nKqIKbhZDf2S29EOI7T1SAVDeqtCZ4i")}, await compile('AddressSaver')));
 
     await addressSaver.sendDeploy(provider.sender(), toNano('0.05'));
 
--- tests/AddressSaver.spec.ts
+++ tests/AddressSaver.spec.ts
@@ -1,8 +1,9 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { beginCell, Cell, toNano } from '@ton/core';
 import { AddressSaver } from '../wrappers/AddressSaver';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import { randomAddress } from '@ton/test-utils';
 
 describe('AddressSaver', () => {
     let code: Cell;
@@ -18,9 +19,9 @@ describe('AddressSaver', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        addressSaver = blockchain.openContract(AddressSaver.createFromConfig({}, code));
-
         deployer = await blockchain.treasury('deployer');
+
+        addressSaver = blockchain.openContract(AddressSaver.createFromConfig({manager: deployer.address}, code));
 
         const deployResult = await addressSaver.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -28,7 +29,7 @@ describe('AddressSaver', () => {
             from: deployer.address,
             to: addressSaver.address,
             deploy: true,
-            success: true,
+            // success: true,
         });
     });
 
@@ -36,4 +37,67 @@ describe('AddressSaver', () => {
         // the check is done inside beforeEach
         // blockchain and addressSaver are ready to use
     });
+
+    it('should change saved address by manager', async ()=>{
+        const address = randomAddress();
+        const result = await addressSaver.sendChangeAddress(deployer.getSender(), toNano('0.01'), 12345n, address);
+
+        expect(result.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: addressSaver.address,
+            success: true
+        })
+    })
+
+    it('should not change saved address by anyone else', async ()=> {
+        let user = await blockchain.treasury('user');
+        const address = randomAddress();
+        const result = await addressSaver.sendChangeAddress(user.getSender(), toNano('0.01'), 12345n, address);
+
+        expect(result.transactions).toHaveTransaction({
+            from: user.address,
+            to: addressSaver.address,
+            success: false,
+            exitCode: 1001
+        })
+    })
+
+    it("should return required data on `requestAddress` call", async () => {
+        const address = randomAddress();
+        await addressSaver.sendChangeAddress(deployer.getSender(), toNano("0.01"), 12345n, address);
+
+        let user = await blockchain.treasury('user');
+        const result = await addressSaver.sendRequestAddress(user.getSender(), toNano('0.01'), 12345n);
+
+        expect(result.transactions).toHaveTransaction({
+            from: addressSaver.address,
+            to: user.address,
+            body: beginCell()
+                .storeUint(3, 32)
+                .storeUint(12345n, 64)
+                .storeAddress(deployer.address)
+                .storeAddress(address)
+                .endCell(),
+        })
+    })
+
+    it('should throw on any other opcode', async () => {
+        let user = await blockchain.treasury('user');
+
+        const result  = await user.send({
+            to: addressSaver.address,
+            value: toNano('0.01'),
+            body: beginCell().storeUint(5, 32).storeUint(12345n, 64).endCell()
+        });
+
+        
+
+        expect(result.transactions).toHaveTransaction({
+            from: user.address,
+            to: addressSaver.address,
+            exitCode: 3
+        })
+    })
+
+    
 });
